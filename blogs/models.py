86be41c591039743d368cd0664bd499100550da0
DANIEL RODRIGUEZ ALFONSO <daniel.rodriguez.alfonso@gmail.com>
2018-11-25 23:26:44 +0100

    Creamos el modelo blog con sus migraciones

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Creamos el modelo de Blog
+
+class Blog(models.Model):
+
+    # owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
